ff1b161b9d8b12fdf6ef459b9dafb48204d046c7
dohyun97 <dohyun647@gmail.com>
2023-04-20 19:44:24 -0400

    JPA

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,14 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "item_name", length = 10) //varchar(10) 스프링에서 객체 필드의 카멜케이스를 테이블컬럼의 언더스코어로 자동으로 변화해줘서 이름부분을 안써도돼
     private String itemName;
     private Integer price;
     private Integer quantity;
